59259c09a497022e49ecd278bdc44fa3c471e5af
GretchH <gretchell.h@gmail.com>
2021-12-11 17:39:55 -0500

    library with grid and library grid

---
+++ src/components/library/librarycourse.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class LibraryCourse extends Component {
+    render() {
+        return (
+            <div className="library-course">
+                <label className="library-course_title">Problem solving</label>
+                {/* icon component */}
+                {/* arrow component */}
+                {/* action button component */}
+                <div className="library-course_description">
+                    <label>Course Description</label>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vitae urna pellentesque, accumsan arcu non, tincidunt ex. Nam at justo eget risus finibus malesuada.</p>
+                    
+                </div>
+            </div>
+        )
+    }
+}
