6cff70566867ba6e2e5e73360a5f2430fc41dedb
GretchH <gretchell.h@gmail.com>
2022-02-07 19:20:17 -0500

    implemented toggleenrolled reducer functionality with JavaScript's Map function

--- src/components/action.js
+++ src/components/action.js
@@ -4,11 +4,13 @@ import Icon from './icon';
 class Action extends Component {
     render() {
         return (
-            <div className={`${this.props.className} action`}>
+            //<div className={`${this.props.className} action`}>
+            <button onClick={() => this.props.onClick()} className={`${this.props.className} action`}>
                 {Icon('fas fa-plus-circle')}
                 {/* fas fa-times-circle*/}
+            </button>
 
-            </div>
+            //</div>
         )
     }
 }
